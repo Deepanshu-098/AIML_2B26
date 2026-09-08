c7d7944d022a5fd3b049ad211f727ffd1751893e
Deepanshu Pratap Singh <dpratap590@gmail.com>
2026-09-08 12:18:40 +0530

    object literals

---
+++ literals.js
@@ -0,0 +1,44 @@
+// Object literals
+
+
+{/* <script>
+let Name = "Deepanshu"
+let rollno = "60"
+let data = {
+    Name,
+    rollno
+}
+console.log(data.name)
+console.log(data.rollno)
+</script> */}
+
+
+{/* <script>
+    let F= "FirstName"
+    let L= "LastName"
+
+    let data ={
+        [F]: "Deepanshu",
+        [L]: "Singh"
+    }
+    console.log(data.FirstName)
+    console.log(data.LastName)
+</script> */}
+
+
+
+
+    let F= "First"
+    let L= "Last"
+
+    let data ={
+        [F]: "Deepanshu",
+        [L]: "Singh"
+    }
+    Show();
+    {
+    console.log(this.First)
+    console.log(this.Last)
+    }
+    console.log(data.Show())
+
